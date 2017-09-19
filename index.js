dffedc714bf0b6731b9f492fc6c8253bc0de767e
Kayla Hansen <shiverr@gmail.com>
2017-09-19 03:11:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ if (top < GAME_HEIGHT) {
  */
 function endGame() {
   clearInterval(gameInterval);
-  ROCKS.remove();
+  ROCKS.push(rock).remove();
   window.removeEventListener('keydown', moveDodger);
   alert("YOU LOSE!")
 }
